06ea15b0d344179e2b0de3f3628be18eeaa862f9
IrfanSahruli <irfansahruli244@gmail.com>
2024-08-20 23:35:02 +0700

    Membuat middleware verify token, membuat  router untuk register, login, dan logout

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
+const express = require('express');
+const dotenv = require('dotenv')
+const sequelize = require('./config/koneksi');
+const Users = require('./models/users');
+const cookieParser = require('cookie-parser');
 
-// dotenv for call value in file .env
-dotenv.config();
 const app = express();
 
+dotenv.config();
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 app.use(cookieParser());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
 
 // this for call all router from router.js
 app.use("/api", require("./routes/router"));
 
-// this for show message if we access port
-app.get("/", (req, res) => {
-  res.json({ msg: "Hello World!" });
-});
+sequelize.authenticate()
+.then(async () => {
+    console.log('Connection success');
+    // await Users.sync({alter:true});
+})
+.catch(err => console.log('Error: ' + err));
 
-app.listen(process.env.PORT, async () => {
-  console.log("server up and running on port " + process.env.PORT);
-});
-
-module.exports = app;
+app.listen(process.env.PORT, () => {
+    console.log(`Server berhasil di running di port ${process.env.PORT}`);
+})
--- routes/testRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-router.get("/test", (req, res) => {
-  res.json({ message: "Test route is working" });
-});
-
-module.exports = router;
