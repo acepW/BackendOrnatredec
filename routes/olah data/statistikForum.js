08d198c05ac12dec328dffddc3088830035e22c0
IrfanSahruli <irfansahruli244@gmail.com>
2024-11-01 15:03:04 +0700

    revisi statistik forum

--- routes/olah data/statistikForum.js
+++ routes/olah data/statistikForum.js
@@ -1,16 +1,9 @@
 const express = require("express");
-const { getForumPostStatisticsForMonth, getForumPostStatisticsForYear, getForumPostStatisticsForYearAll } = require('../../controllers/olah data/statistikForum');
+const { getForumPostStatisticsForYearAll } = require('../../controllers/olah data/statistikForum');
 
 const router = express.Router();
 
 // Route untuk mendapatkan statistik postingan per tahun (semua bulan)
-router.get('/statistik/tahun/:year', getForumPostStatisticsForYearAll);
-
-// Route untuk mendapatkan statistik postingan per tahun dengan perbandingan tahun sebelumnya
-router.get('/statistik/:year', getForumPostStatisticsForYear);
-
-
-// Route untuk mendapatkan statistik postingan per bulan dengan perbandingan bulan sebelumnya
-router.get('/statistik/:year/:month', getForumPostStatisticsForMonth);
+router.get('/statistik/:year', getForumPostStatisticsForYearAll);
 
 module.exports = router;
